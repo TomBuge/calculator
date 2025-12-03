b387843cbf8ee792b39b1cad55d2844eeb117b3d
Tom Bugeja <46567954+TomBuge@users.noreply.github.com.>
2025-12-03 15:37:23 +0000

    add logic for number buttons and clear buttons
    when buttons pressed display changes

--- script.js
+++ script.js
@@ -18,5 +18,38 @@ const operate = (firstNum, secondNum, operator) => {
     case "/":
         return divide(firstNum, secondNum);
     } 
-
 };
+
+let displayNumber = "0"; 
+const display = document.querySelector(".display");
+display.textContent = displayNumber;
+const numberButtons = document.querySelectorAll(".number");
+const pressNumberButton = numberButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (displayNumber === "0") {
+            displayNumber = btn.textContent;
+            display.textContent = displayNumber;
+            console.log(displayNumber);
+        }
+        else {
+            displayNumber += btn.textContent;
+            display.textContent += btn.textContent;
+            console.log(displayNumber);
+        }
+    }) 
+        
+});
+
+const clearButton = document.querySelector("#clear");
+const pressClearButton = clearButton.addEventListener("click", () => {
+    displayNumber = "0";
+    display.textContent = displayNumber;
+});
+
+
+// create display variable 
+// add event listen to all buttons
+// if button has class number get button content 
+// if display variable is empty, store button content. 
+// if display var already has a number, concatenate 
+
